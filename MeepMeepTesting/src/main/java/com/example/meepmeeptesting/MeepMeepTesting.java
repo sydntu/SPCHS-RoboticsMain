c68ff880bf3ba55f3e42c5213e4194e34b0e495a
sauceWHY <129795792+sauceWHY@users.noreply.github.com>
2023-11-13 20:03:27 -0500

    hihi

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,6 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -12,19 +11,17 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(219.476), Math.toRadians(219.476), 13.12)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, 60, Math.toRadians(270)))
-                                .lineToConstantHeading(new Vector2d(-35,15))
-                                .splineToSplineHeading(new Pose2d(50, 34.5, Math.toRadians(0)), Math.toRadians(60))
-
+                                .splineToSplineHeading(new Pose2d(-35, 10, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(47, 35, Math.toRadians(0)), Math.toRadians(20))
                                 .build()
-
                 );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
-                .setBackgroundAlpha(.90f)
+                .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
     }
